4df69b28d4d985192e90ccb81492eace4b6e91e8
denissellu <hingasellu@gmail.com>
2020-11-25 00:22:12 +0000

    Add init files for guessing game'

---
+++ ch02/guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+/Users/denissellu/code_base/rust-lang-book/ch02/guessing_game/target/debug/deps/guessing_game: src/main.rs
+
+/Users/denissellu/code_base/rust-lang-book/ch02/guessing_game/target/debug/deps/guessing_game.d: src/main.rs
+
+src/main.rs:
---
+++ ch02/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/denissellu/code_base/rust-lang-book/ch02/guessing_game/target/debug/guessing_game: /Users/denissellu/code_base/rust-lang-book/ch02/guessing_game/src/main.rs
